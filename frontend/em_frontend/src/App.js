0c85c2affc21a5f38baf1cfb16d3d764a53f42b2
Agar Avarzed <aavarzed24@wooster.edu>
2024-03-08 20:36:15 +0800

    Navbar component started

--- frontend/em_frontend/src/App.js
+++ frontend/em_frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar.jsx'
+
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/em_frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center h-20 '>
+        <div>
+            <h1>
+                Explore Mongolia
+            </h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
