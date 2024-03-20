8a782a12a04191cc077019e8b94edb84dc65c64a
Agar Avarzed <aavarzed24@wooster.edu>
2024-03-20 15:03:19 +0800

    Navbar logics and design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/ExploreMongolia.svg';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  return (
-    <header className='sticky top-0 z-50'>
-        <nav className='flex justify-between items-center h-20 px-4' >
-        <img
-            src = { Logo }
-            alt = 'ExploreMongolia.svg'
-            width={160}
-            height={40}
-        />
-        <ul className='flex'>
-            <li>Tours</li>
-            <li>Blogs</li>
-            <li>About us</li>
-            <li>Contact</li>
-            <li>
-                <button className='bg-black text-white px-4 py-2 rounded full'>
-                    Register
-                </button>
-            </li>
-        </ul>
-        </nav>
-    </header>
-  )
-}
+  const [showMenu, setShowMenu] = useState(false);  // State for menu visibility
+  const [logo, showLogo] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);  // Toggle menu visibility on click
+    showLogo(!showLogo);
+  };
+
+  return (
+    <header className='sticky top-0 z-50 mb-4'>
+      <nav className='flex justify-between items-center h-20 px-4'>
+        <img onClick={toggleMenu} className={logo ? 'hidden' : 'block'} src={Logo} alt='ExploreMongolia.svg' width={160} height={40} />
+        <ul className='flex space-x-4 hidden md:flex'> {/* Hidden on mobile */}
+          <li>
+            <button className='text-black bg-transparent px-4 py-2 rounded-full'>
+                Tours
+            </button>
+          </li>
+          <li>
+            <button className='text-black bg-transparent px-4 py-2 rounded-full'>
+                Blogs
+            </button>
+          </li>
+          <li>
+            <button className='text-black bg-transparent px-4 py-2 rounded-full'>
+                About Us
+            </button>
+          </li>
+          <li>
+            <button className='text-black bg-transparent px-4 py-2 rounded-full ml-4'>
+                Contact
+            </button>
+          </li>
+          <li>
+            <button className='bg-black text-white px-4 py-2 rounded-full ml-4'>
+                Register
+            </button>
+          </li>
+        </ul>
+        
+        {/* Hamburger */}
+        <div onClick={toggleMenu} className='md:hidden z-10 items-center'>
+            {showMenu ? <IoCloseOutline size={20} />  : <RxHamburgerMenu size={20} />}
+        </div>
+
+        {/* Mobile menu dropdown */}
+        <div onClick={toggleMenu} className={showMenu ? 'absolute left-0 top-0 w-full bg-gray-100 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+            <img src={Logo} alt='ExploreMongolia.svg' width={160} height={40} />
+            <ul>
+                <li className='border-b'>
+                    Tours
+                </li>
+                <li className='border-b'>
+                    Blogs
+                </li>
+                <li className='border-b'>
+                    About Us
+                </li>
+                <li className='border-b'>
+                    Contact
+                </li>
+                <div className='flex flex-col'>
+                    <li>
+                        <button className='bg-black text-white px-4 py-2 rounded-full ml-4'>
+                            Register
+                        </button>
+                    </li>
+                </div>
+                
+            </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
